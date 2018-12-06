26818e5a23ea37a2dd220a9349c207cf043d9cf0
goodee <goodee@DESKTOP-OBOOCN7>
2018-12-06 13:54:45 +0900

    ....

--- Sails/src/main/java/com/gd/sail/web/test/controller/sailcontroller.java
+++ Sails/src/main/java/com/gd/sail/web/test/controller/sailcontroller.java
@@ -152,7 +152,6 @@ public class sailcontroller {
 		
 		return modelAndView;
 	}
-<<<<<<< HEAD
 	@RequestMapping(value="/SignAjax" , method = RequestMethod.POST, produces = "test/json;charset=UTF-8")
 	@ResponseBody
 	public String SignAjax(@RequestParam HashMap<String,String> param)throws Throwable{
@@ -173,8 +172,7 @@ public class sailcontroller {
 		
 		return "";
 	}
-=======
->>>>>>> branch 'master' of https://github.com/kiva095/sails.git
+	
 	@RequestMapping(value="/SampleMain2")
 	public ModelAndView SampleMain2(
 			@RequestParam HashMap<String,String> params,
